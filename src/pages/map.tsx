4c86419ec02170df111f7fe036a24a5ce644032c
Tristan Rechenberger <rechenberger@me.com>
2023-07-02 00:26:49 +0200

    🎉

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -127,7 +127,7 @@ export default function Page() {
                 longitude={point[1]}
                 anchor="center"
               >
-                <div className="relative aspect-square rounded-full bg-blue-500 p-0.5">
+                <div className="relative aspect-square rounded-full border border-white bg-blue-500 p-0.5">
                   <Footprints size={12} className="animate text-white" />
                   {/* <div className="absolute inset-0 animate-ping rounded-full ring-2 ring-blue-400" /> */}
                 </div>
