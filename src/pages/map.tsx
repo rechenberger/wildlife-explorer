a4dd8aeb0c194cb34df2a5b35e905600e53d1dd2
Tristan Rechenberger <rechenberger@me.com>
2023-07-01 22:13:50 +0200

    🎉

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -11,12 +11,12 @@ export default function Page() {
           initialViewState={{
             latitude: 50.928435947011906,
             longitude: 6.930087265110956,
-            // pitch: 45,
+            pitch: 45,
             zoom: 15,
           }}
           style={{ width: "100%", height: "100%" }}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
-          // mapStyle="mapbox://styles/rechenberger/cljkelien006n01o429b9440e"
+          // mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapStyle="mapbox://styles/rechenberger/cljkelien006n01o429b9440e"
           mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_TOKEN}
           onMove={(e) => console.log(e)}
         >
