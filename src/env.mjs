f6f9f1c60b95f9679734d8340d336a902bf94831
Tristan Rechenberger <rechenberger@me.com>
2023-07-02 22:54:17 +0200

    🎉

--- src/env.mjs
+++ src/env.mjs
@@ -7,7 +7,8 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL_POOL: z.string().url(),
+    DATABASE_URL_DIRECT: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === "production"
@@ -39,7 +40,8 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL_DIRECT: process.env.DATABASE_URL_DIRECT,
+    DATABASE_URL_POOL: process.env.DATABASE_URL_POOL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
