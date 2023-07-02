496ac2a65ebe10c4b6fb13c362b2b9ff0000721d
Tristan Rechenberger <rechenberger@me.com>
2023-07-02 12:34:18 +0200

    🎉

--- src/client/MainTopNav.tsx
+++ src/client/MainTopNav.tsx
@@ -1,4 +1,5 @@
 import { Carter_One } from "next/font/google"
+import { BrushStrokeBackground } from "./BrushStrokeBackground"
 
 const carterOne = Carter_One({
   weight: "400",
@@ -8,11 +9,13 @@ const carterOne = Carter_One({
 export const MainTopNav = () => {
   return (
     <>
-      <div className="fixed inset-x-0 top-0 z-10 flex flex-row px-4 py-8 text-amber-400">
+      <div className="fixed inset-x-0 top-0 z-10 flex flex-row px-4 py-8">
         <div className="flex-1"></div>
-        <h1 className="text-6xl [text-shadow:_1px_1px_4px_rgb(0_0_0_/_80%)]">
-          <span className={carterOne.className}>Wildlife Explorer</span>
-        </h1>
+        <BrushStrokeBackground>
+          <h1 className="text-6xl text-amber-400 [text-shadow:_1px_1px_4px_rgb(0_0_0_/_80%)]">
+            <span className={carterOne.className}>Wildlife Explorer</span>
+          </h1>
+        </BrushStrokeBackground>
         <div className="flex-1"></div>
       </div>
     </>
