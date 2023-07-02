b997f2265c11a7a04465700f37e4153392525b8f
Tristan Rechenberger <rechenberger@me.com>
2023-07-02 12:52:55 +0200

    🎉

---
+++ src/client/LoginButton.tsx
@@ -0,0 +1,28 @@
+import { signIn, signOut, useSession } from "next-auth/react"
+
+export const LoginButton = () => {
+  const session = useSession()
+
+  if (!session.data)
+    return (
+      <>
+        <button
+          className="rounded bg-black/80 px-2 py-1"
+          onClick={() => signIn()}
+        >
+          Login
+        </button>
+      </>
+    )
+
+  return (
+    <>
+      <button
+        className="rounded bg-black/80 px-2 py-1"
+        onClick={() => signOut()}
+      >
+        Logout
+      </button>
+    </>
+  )
+}
--- src/client/MainTopNav.tsx
+++ src/client/MainTopNav.tsx
@@ -1,5 +1,6 @@
 import { Carter_One } from "next/font/google"
 import { BrushStrokeBackground } from "./BrushStrokeBackground"
+import { LoginButton } from "./LoginButton"
 
 const carterOne = Carter_One({
   weight: "400",
@@ -10,13 +11,15 @@ export const MainTopNav = () => {
   return (
     <>
       <div className="fixed inset-x-0 top-0 z-10 flex flex-row px-4 py-8">
-        <div className="flex-1"></div>
+        <div className="flex flex-1 flex-row items-center justify-start"></div>
         <BrushStrokeBackground>
           <h1 className="text-6xl text-amber-400 [text-shadow:_1px_1px_4px_rgb(0_0_0_/_80%)]">
             <span className={carterOne.className}>Wildlife Explorer</span>
           </h1>
         </BrushStrokeBackground>
-        <div className="flex-1"></div>
+        <div className="flex flex-1 flex-row items-center justify-end">
+          <LoginButton />
+        </div>
       </div>
     </>
   )
