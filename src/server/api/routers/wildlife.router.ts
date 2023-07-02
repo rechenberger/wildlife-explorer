18e284419770440a36a954868a360336c7b55cca
Tristan Rechenberger <rechenberger@me.com>
2023-07-02 13:01:09 +0200

    🎉

--- src/server/api/routers/wildlife.router.ts
+++ src/server/api/routers/wildlife.router.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server"
 import { z } from "zod"
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
 import { Observation } from "~/server/inaturalist/schema"
@@ -12,15 +13,24 @@ export const wildlifeRouter = createTRPCRouter({
       const url = `https://api.inaturalist.org/v1/observations?taxon_id=1&lat=${lat}&lng=${lng}&radius=${radiusInKm}&order=desc&order_by=created_at&per_page=200&locale=de`
       console.log(url)
       const response = await fetch(url)
-      const data = await response.json()
-      const schema = z.object({
-        total_results: z.number(),
-        page: z.number(),
-        per_page: z.number(),
-        results: z.array(Observation),
-      })
-      const parsed = schema.parse(data)
-      console.log(parsed.total_results, parsed.results.length)
-      return parsed
+      try {
+        const data = await response.json()
+        const schema = z.object({
+          total_results: z.number(),
+          page: z.number(),
+          per_page: z.number(),
+          results: z.array(Observation),
+        })
+        const parsed = schema.parse(data)
+        console.log(parsed.total_results, parsed.results.length)
+        return parsed
+      } catch (error) {
+        const text = await response.text()
+        console.log(text)
+        throw new TRPCError({
+          code: "PARSE_ERROR",
+          message: text,
+        })
+      }
     }),
 })
