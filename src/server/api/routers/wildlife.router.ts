5855c2f38fdf17c82dd4f50e28176db7a6817200
Tristan Rechenberger <rechenberger@me.com>
2023-07-02 14:43:31 +0200

    🎉

--- src/server/api/routers/wildlife.router.ts
+++ src/server/api/routers/wildlife.router.ts
@@ -8,32 +8,43 @@ export const wildlifeRouter = createTRPCRouter({
       z.object({ lat: z.number(), lng: z.number(), radiusInKm: z.number() })
     )
     .query(async ({ input }) => {
-      const { lat, lng, radiusInKm } = input
-      const url = `https://api.inaturalist.org/v1/observations?taxon_id=1&lat=${lat}&lng=${lng}&radius=${radiusInKm}&order=desc&order_by=created_at&per_page=200&locale=de`
-      console.log(url)
-      const response = await fetch(url)
-      const data = await response.json()
-      const schema = z.object({
-        total_results: z.number(),
-        page: z.number(),
-        per_page: z.number(),
-        results: z.array(Observation),
-      })
-      const parsed = schema.parse(data)
-      console.log(parsed.total_results, parsed.results.length)
-
-      const wildlifes = parsed.results.map((o) => {
-        return {
-          id: o.id,
-          name: o.taxon.preferred_common_name || o.taxon.name,
-          lat: o.geojson.coordinates[1],
-          lng: o.geojson.coordinates[0],
-          imgUrl: o.taxon.default_photo?.square_url,
-          observationUrl: o.uri,
-          wikiUrl: o.taxon.wikipedia_url,
-        }
-      })
-
-      return wildlifes
+      return findWildlife(input)
     }),
 })
+
+const findWildlife = async ({
+  lat,
+  lng,
+  radiusInKm,
+}: {
+  lat: number
+  lng: number
+  radiusInKm: number
+}) => {
+  const url = `https://api.inaturalist.org/v1/observations?taxon_id=1&lat=${lat}&lng=${lng}&radius=${radiusInKm}&order=desc&order_by=created_at&per_page=200&locale=de`
+  console.log(url)
+  const response = await fetch(url)
+  const data = await response.json()
+  const schema = z.object({
+    total_results: z.number(),
+    page: z.number(),
+    per_page: z.number(),
+    results: z.array(Observation),
+  })
+  const parsed = schema.parse(data)
+  console.log(parsed.total_results, parsed.results.length)
+
+  const wildlifes = parsed.results.map((o) => {
+    return {
+      id: o.id,
+      name: o.taxon.preferred_common_name || o.taxon.name,
+      lat: o.geojson.coordinates[1],
+      lng: o.geojson.coordinates[0],
+      imgUrl: o.taxon.default_photo?.square_url,
+      observationUrl: o.uri,
+      wikiUrl: o.taxon.wikipedia_url,
+    }
+  })
+
+  return wildlifes
+}
