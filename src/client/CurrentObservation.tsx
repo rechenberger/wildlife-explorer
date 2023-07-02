0c6dc90a3e05767d53a3bee5f2d8657eaccf8db1
Tristan Rechenberger <rechenberger@me.com>
2023-07-02 16:02:07 +0200

    🎉

--- src/client/CurrentObservation.tsx
+++ src/client/CurrentObservation.tsx
@@ -1,4 +1,5 @@
-import { atom, useAtomValue } from "jotai"
+import { atom, useAtomValue, useSetAtom } from "jotai"
+import { X } from "lucide-react"
 import Image from "next/image"
 import { Away } from "./Away"
 import { useWildlife } from "./WildlifeMarkers"
@@ -9,6 +10,7 @@ export const currentObservationIdAtom = atom<number | null>(null)
 export const CurrentObservation = () => {
   const { wildlife } = useWildlife()
   const currentObservationId = useAtomValue(currentObservationIdAtom)
+  const setCurrentObservationId = useSetAtom(currentObservationIdAtom)
 
   const w = wildlife?.find((w) => w.id === currentObservationId)
 
@@ -20,8 +22,16 @@ export const CurrentObservation = () => {
 
   return (
     <>
-      <div className="fixed bottom-8 right-8 flex w-full max-w-md flex-col gap-4 rounded-xl bg-white/80 p-4 text-black shadow">
-        <div className="text-2xl">{w.name}</div>
+      <div className="fixed bottom-8 right-0 flex w-full max-w-md flex-col gap-4 rounded-xl bg-white p-4 text-black shadow md:right-8">
+        <div className="flex flex-row items-center gap-2">
+          <div className="flex-1 truncate text-2xl">{w.name}</div>
+          <button
+            className="shrink-0"
+            onClick={() => setCurrentObservationId(null)}
+          >
+            <X size={16} />
+          </button>
+        </div>
         <div className="-mt-2">{location && <Away location={location} />}</div>
         {w.imgUrlMedium && (
           <div className="-mx-4">
