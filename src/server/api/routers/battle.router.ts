c9ac5a8c9b09ba0854694d1b968c042a15b38440
Tristan Rechenberger <rechenberger@me.com>
2023-07-07 17:28:29 +0200

    ✨ attackWildlife

--- src/server/api/routers/battle.router.ts
+++ src/server/api/routers/battle.router.ts
@@ -1,34 +1,27 @@
-import { z } from "zod"
 import { createTRPCRouter } from "~/server/api/trpc"
-import { playerProcedure } from "../middleware/playerProcedure"
+import { wildlifeProcedure } from "../middleware/wildlifeProcedure"
 
 export const battleRouter = createTRPCRouter({
-  attackWildlife: playerProcedure
-    .input(
-      z.object({
-        wildlifeId: z.string(),
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      await ctx.prisma.battle.create({
-        data: {
-          status: "IN_PROGRESS",
-          metadata: {},
-          battleParticipants: {
-            createMany: {
-              data: [
-                {
-                  playerId: ctx.player.id,
-                  metadata: {},
-                },
-                {
-                  wildlifeId: input.wildlifeId,
-                  metadata: {},
-                },
-              ],
-            },
+  attackWildlife: wildlifeProcedure.mutation(async ({ ctx }) => {
+    await ctx.prisma.battle.create({
+      data: {
+        status: "IN_PROGRESS",
+        metadata: {},
+        battleParticipants: {
+          createMany: {
+            data: [
+              {
+                playerId: ctx.player.id,
+                metadata: {},
+              },
+              {
+                wildlifeId: ctx.wildlife.id,
+                metadata: {},
+              },
+            ],
           },
         },
-      })
-    }),
+      },
+    })
+  }),
 })
