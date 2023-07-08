d5779228d192e2aad14c39de3948faf0d48bdbfa
Tristan Rechenberger <rechenberger@me.com>
2023-07-08 15:46:06 +0200

    ✨ battle

--- src/server/lib/battle/getWildlifeFighter.ts
+++ src/server/lib/battle/getWildlifeFighter.ts
@@ -42,6 +42,11 @@ export const getWildlifeFighter = async ({
     seed: [seed, "nature"],
   })
 
+  const gender = rngItem({
+    items: ["M", "F"],
+    seed: [seed, "gender"],
+  })
+
   return {
     ...base,
     name: wildlife.id.substring(0, MAX_NAME_LENGTH),
@@ -49,6 +54,47 @@ export const getWildlifeFighter = async ({
     moves,
     level,
     nature: nature.name,
+    ivs: {
+      hp: rngInt({
+        seed: [seed, "iv", "hp"],
+        min: 0,
+        max: 31,
+      }),
+      atk: rngInt({
+        seed: [seed, "iv", "atk"],
+        min: 0,
+        max: 31,
+      }),
+      def: rngInt({
+        seed: [seed, "iv", "def"],
+        min: 0,
+        max: 31,
+      }),
+      spa: rngInt({
+        seed: [seed, "iv", "spa"],
+        min: 0,
+        max: 31,
+      }),
+      spd: rngInt({
+        seed: [seed, "iv", "spd"],
+        min: 0,
+        max: 31,
+      }),
+      spe: rngInt({
+        seed: [seed, "iv", "spe"],
+        min: 0,
+        max: 31,
+      }),
+    },
+    evs: {
+      hp: 0,
+      atk: 0,
+      def: 0,
+      spa: 0,
+      spd: 0,
+      spe: 0,
+    },
+    gender,
   }
 }
 
