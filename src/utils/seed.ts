5c975ae679552e7e849ec7c710c2a7fae590b1f5
Tristan Rechenberger <rechenberger@me.com>
2023-07-08 15:27:55 +0200

    ✨ battle

--- src/utils/seed.ts
+++ src/utils/seed.ts
@@ -1,3 +1,6 @@
+import { isArray } from "lodash-es"
+import seedrandom from "seedrandom"
+
 export const createSeed = ({
   observationId,
   respawnsAt,
@@ -6,4 +9,36 @@ export const createSeed = ({
   respawnsAt: Date
 }) => {
   return `${observationId}-${respawnsAt.toISOString()}`
+}
+
+type RngSeed = string | (string | number)[]
+
+export const rng = ({ seed }: { seed: RngSeed }) => {
+  const seedString = isArray(seed) ? seed.join("~") : seed
+  const generator = seedrandom(seedString)
+  return generator()
+}
+
+export const rngInt = ({
+  seed,
+  min = 0,
+  max,
+}: {
+  seed: RngSeed
+  min?: number
+  max: number
+}) => {
+  const rnd = rng({ seed })
+  return Math.floor(rnd * (max - min + 1)) + min
+}
+
+export const rngItem = <T>({
+  seed,
+  items,
+}: {
+  seed: RngSeed
+  items: T[]
+}): T => {
+  const idx = rngInt({ seed, max: items.length - 1 })
+  return items[idx]!
 }
