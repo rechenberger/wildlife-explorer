0abc248164c7ca646b9234ea104ecdb4d7406530
Tristan Rechenberger <rechenberger@me.com>
2023-07-09 18:19:20 +0200

    💄 modal

--- src/client/MainActionButtons.tsx
+++ src/client/MainActionButtons.tsx
@@ -4,7 +4,7 @@ import { ScanButton } from "./ScanButton"
 export const MainActionButtons = () => {
   return (
     <>
-      <div className="absolute inset-x-8 bottom-8 z-40 flex flex-row justify-center gap-2">
+      <div className="absolute inset-x-2 bottom-1 z-40 flex flex-row justify-center gap-4">
         <ScanButton />
         <BattleViewButton />
         {/* <TeleportToCurrentPositionButton /> */}
