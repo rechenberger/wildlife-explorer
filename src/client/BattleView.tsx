4f294c51aeebc509205e4719a9322d08c0f99e57
Tristan Rechenberger <rechenberger@me.com>
2023-07-09 20:33:01 +0200

    💄

--- src/client/BattleView.tsx
+++ src/client/BattleView.tsx
@@ -574,14 +574,14 @@ const TypeBadge = ({
       <div
         title={title || content}
         className={cn(
-          "flex items-center gap-1 rounded-md pr-2 text-sm",
+          "flex items-center gap-1 rounded-md pr-2 text-xs",
           icon.bgHalf,
           !icon.icon && "pl-1"
         )}
       >
         {icon.icon && (
           <div className={cn("rounded-md p-1", icon.bgFull)}>
-            <icon.icon className="h-4 w-4" />
+            <icon.icon className="h-3 w-3" />
           </div>
         )}
         <div>{content}</div>
