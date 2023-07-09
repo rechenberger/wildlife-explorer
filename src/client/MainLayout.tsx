bb19b12ed70fea080be4521f5e3dbd6c25d9a848
Tristan Rechenberger <rechenberger@me.com>
2023-07-09 22:13:02 +0200

    📱

--- src/client/MainLayout.tsx
+++ src/client/MainLayout.tsx
@@ -9,6 +9,11 @@ export const MainLayout = ({ children }: { children?: ReactNode }) => {
         <title>Wildlife Explorer</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="theme-color" content="#72A5A6"></meta>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, viewport-fit=cover"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
       <main className="relative flex h-[100svh] w-full flex-col items-center justify-center bg-gradient-to-br  from-cyan-500 to-amber-600 text-white">
         <MainTopNav />
