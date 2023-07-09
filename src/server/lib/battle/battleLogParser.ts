be1a6c0337d560deb255d0587a09476fae96d917
Tim Weiskopf <weiskopf@sodefa.de>
2023-07-09 21:40:08 +0200

    ♻️ rename const

--- src/server/lib/battle/battleLogParser.ts
+++ src/server/lib/battle/battleLogParser.ts
@@ -2,13 +2,13 @@ import { Protocol } from "@pkmn/protocol"
 import { LogFormatter } from "@pkmn/view"
 
 export const parseBattleLog = (line?: string | string[] | undefined) => {
-  const xy = new LogFormatter()
+  const formatter = new LogFormatter()
 
   const extractMessage = (buf: string) => {
     let out = ""
     for (const line of buf.split("\n")) {
       const { args, kwArgs } = Protocol.parseBattleLine(line)
-      out += xy.formatText(args, kwArgs)
+      out += formatter.formatText(args, kwArgs)
     }
     return out
   }
