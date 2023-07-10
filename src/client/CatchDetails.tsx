4a51b89d8a2973258cfaf26cd0339b1a6287e2a5
Tristan Rechenberger <rechenberger@me.com>
2023-07-10 23:09:59 +0200

    💄

--- src/client/CatchDetails.tsx
+++ src/client/CatchDetails.tsx
@@ -21,9 +21,15 @@ export const CatchDetails = ({ catchId }: { catchId: string }) => {
     <>
       <div>{getName(c.wildlife)}</div>
       <div className="p-2 flex flex-col gap-4">
+        <div className="flex flex-row gap-2 items-center text-xs font-bold opacity-60 mt-4">
+          <hr className="flex-1 border-black/60" />
+          <div>Wildlife</div>
+          <hr className="flex-1 border-black/60" />
+        </div>
         <div className="w-1/2">
           <FighterChip showAbsoluteHp ltr fighter={c} />
         </div>
+
         <div className="flex flex-row gap-2 items-center text-xs font-bold opacity-60 mt-4">
           <hr className="flex-1 border-black/60" />
           <div>Types, Ability, Nature</div>
@@ -39,12 +45,14 @@ export const CatchDetails = ({ catchId }: { catchId: string }) => {
             className="flex-1"
           />
         </div>
+
         <div className="flex flex-row gap-2 items-center text-xs font-bold opacity-60 mt-4">
           <hr className="flex-1 border-black/60" />
           <div>Moves</div>
           <hr className="flex-1 border-black/60" />
         </div>
         <FighterMoves fighter={c} />
+
         <div className="flex flex-row gap-2 items-center text-xs font-bold opacity-60 mt-4">
           <hr className="flex-1 border-black/60" />
           <div>Stats</div>
