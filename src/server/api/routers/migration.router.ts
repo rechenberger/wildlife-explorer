267f94b152c1f38d5ffb369ec4325dadb387eb22
Tim Weiskopf <weiskopf@sodefa.de>
2023-07-12 00:14:17 +0200

    ✨ mutate button for catchmetadata

--- src/server/api/routers/migration.router.ts
+++ src/server/api/routers/migration.router.ts
@@ -1,5 +1,10 @@
 import { MAX_FIGHTERS_PER_TEAM } from "~/config"
+import { PokemonExperienceMap } from "~/data/pokemonLevelExperienceMap"
+import { PokemonLevelingRate } from "~/data/pokemonLevelingRate"
 import { createTRPCRouter } from "~/server/api/trpc"
+import { getWildlifeFighterPlus } from "~/server/lib/battle/getWildlifeFighterPlus"
+import { CatchMetadata, LevelingRate } from "~/server/schema/CatchMetdata"
+import { WildlifeMetadata } from "~/server/schema/WildlifeMetadata"
 import { devProcedure } from "../middleware/devProcedure"
 
 export const migrationRouter = createTRPCRouter({
@@ -10,6 +15,51 @@ export const migrationRouter = createTRPCRouter({
     })
     console.log(wildlife)
     return wildlife
+  }),
+
+  catchMetadata: devProcedure.mutation(async ({ ctx }) => {
+    const catches = await ctx.prisma.catch.findMany({
+      include: {
+        wildlife: true,
+      },
+    })
+
+    for (const c of catches) {
+      const wildlife = {
+        ...c.wildlife,
+        metadata: WildlifeMetadata.parse(c.wildlife.metadata),
+      }
+      const wildlifeFighterPlus = await getWildlifeFighterPlus({
+        wildlife,
+        seed: c.seed,
+      })
+
+      const speciesName = wildlifeFighterPlus.species
+      const speciesNum = wildlifeFighterPlus.speciesDefinition.num
+      const level = wildlifeFighterPlus.level
+      const levelingRate = LevelingRate.parse(
+        PokemonLevelingRate[speciesNum]?.levelingRate
+      )
+      const baseExp =
+        PokemonExperienceMap[`${level}-${levelingRate}`]?.requiredExperience
+
+      const catchMetadata = {
+        speciesNum,
+        level,
+        exp: baseExp,
+        levelingRate,
+        speciesName,
+      } satisfies CatchMetadata
+
+      await ctx.prisma.catch.update({
+        where: {
+          id: c.id,
+        },
+        data: {
+          metadata: catchMetadata,
+        },
+      })
+    }
   }),
 
   battleOrder: devProcedure.mutation(async ({ ctx }) => {
