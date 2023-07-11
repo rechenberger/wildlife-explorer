234ae4f5fd8ed72f449fed06106e1c12d9d4e769
Tristan Rechenberger <rechenberger@me.com>
2023-07-11 19:52:13 +0200

    ✨ pvp

--- src/server/schema/BattleParticipationMetadata.ts
+++ src/server/schema/BattleParticipationMetadata.ts
@@ -1,6 +1,8 @@
 import { z } from "zod"
 
-export const BattleParticipationMetadata = z.object({})
+export const BattleParticipationMetadata = z.object({
+  nextChoice: z.string().nullish(),
+})
 export type BattleParticipationMetadata = z.infer<
   typeof BattleParticipationMetadata
 >
