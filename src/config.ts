e600948d506857051c0eaf6aecb1a4658e4cd4af
Tristan Rechenberger <rechenberger@me.com>
2023-07-14 11:25:06 +0200

    🔧 DEV_MODE

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,8 @@
-export const GLOBAL_REALTIME_MULTIPLIER = 40
+export const DEV_MODE =
+  process.env.NODE_ENV === "development" ||
+  (typeof localStorage !== "undefined" && !!localStorage.getItem("DEV_MODE"))
+
+export const GLOBAL_REALTIME_MULTIPLIER = DEV_MODE ? 400 : 40
 export const DEFAULT_LOCATION = {
   lat: 50.928435947011906,
   lng: 6.930087265110956,
@@ -8,21 +12,18 @@ export const RADIUS_IN_KM_SEE_WILDLIFE = 5
 export const RADIUS_IN_KM_SCAN_WILDLIFE_SMALL = 0.5
 export const RADIUS_IN_KM_SCAN_WILDLIFE_BIG = 5
 export const RADIUS_IN_M_CATCH_WILDLIFE = 500
-export const DEFAULT_RESPAWN_TIME_IN_MINUTES = 10
+export const DEFAULT_RESPAWN_TIME_IN_MINUTES = DEV_MODE ? 10 / 60 : 10
 export const WILDLIFE_REFETCH_INTERVAL_IN_MS = 3_000
 export const OTHER_PLAYER_REFETCH_INTERVAL_IN_MS = 1_000
 export const SHOW_OBSERVATION_JSON =
   false && process.env.NODE_ENV === "development"
-export const SCAN_COOLDOWN_IN_SECONDS = 20
+export const SCAN_COOLDOWN_IN_SECONDS = DEV_MODE ? 1 : 20
 export const DEFAULT_DB_CHUNK_SIZE = 20
 export const MAX_METER_BY_FOOT = 2_000
 export const MAX_METER_BY_BICYCLE = 10_000
 export const MIN_METER_ACCURACY_SHOW_INACCURATE = 200
 export const MAX_FIGHTERS_PER_TEAM = 6
 export const MAX_MOVES_PER_FIGHTER = 4
-export const DEV_MODE =
-  process.env.NODE_ENV === "development" ||
-  (typeof localStorage !== "undefined" && !!localStorage.getItem("DEV_MODE"))
 
 export const ENABLE_BATTLE_VIEW = true
 
