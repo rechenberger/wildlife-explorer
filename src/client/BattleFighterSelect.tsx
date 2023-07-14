f34a80c300285a88e01c4a04694e3e005348e367
Tristan Rechenberger <rechenberger@me.com>
2023-07-14 18:41:20 +0200

    💄 BattleFighterSelect

--- src/client/BattleFighterSelect.tsx
+++ src/client/BattleFighterSelect.tsx
@@ -1,9 +1,25 @@
+import { Fragment } from "react"
 import { type BattleReportFighter } from "~/server/lib/battle/BattleReport"
+import { CatchDetails } from "./CatchDetails"
 
 export const BattleFighterSelect = ({
   fighters,
 }: {
   fighters: BattleReportFighter[]
 }) => {
-  return null
+  return (
+    <>
+      <div className="">Select your next Fighter</div>
+      <div className="flex flex-col gap-2">
+        {fighters.map((fighter, idx) => {
+          if (!fighter.catch) return null
+          return (
+            <Fragment key={idx}>
+              <CatchDetails catchId={fighter.catch.id} tiny />
+            </Fragment>
+          )
+        })}
+      </div>
+    </>
+  )
 }
