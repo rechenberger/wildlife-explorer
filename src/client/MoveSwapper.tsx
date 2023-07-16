09fff0dcbfdd07aab16e22eb19cdbef10a296c86
Tristan Rechenberger <rechenberger@me.com>
2023-07-16 11:13:29 +0200

    ✨ MoveSwapper

--- src/client/MoveSwapper.tsx
+++ src/client/MoveSwapper.tsx
@@ -42,29 +42,37 @@ export const MoveSwapper = ({ catchId }: { catchId: string }) => {
           )
         })}
       </div>
-      <DividerHeading>Learned Moves</DividerHeading>
-      <div className="grid flex-1 grid-cols-1 gap-1">
-        {map(learned, (move) => {
-          return (
-            <Fragment key={move.id}>
-              <FighterMove fighter={c} move={move} />
-            </Fragment>
-          )
-        })}
-      </div>
-      <DividerHeading>Future Moves</DividerHeading>
-      <div className="grid flex-1 grid-cols-[auto_1fr] gap-1 items-center gap-x-2">
-        {map(future, (move) => {
-          return (
-            <Fragment key={move.id}>
-              <div className="text-gray-500 text-sm">
-                Level {move.learnAtLevel}
-              </div>
-              <FighterMove fighter={c} move={move} />
-            </Fragment>
-          )
-        })}
-      </div>
+      {!!learned?.length && (
+        <>
+          <DividerHeading>Learned Moves</DividerHeading>
+          <div className="grid flex-1 grid-cols-1 gap-1">
+            {map(learned, (move) => {
+              return (
+                <Fragment key={move.id}>
+                  <FighterMove fighter={c} move={move} />
+                </Fragment>
+              )
+            })}
+          </div>
+        </>
+      )}
+      {!!future?.length && (
+        <>
+          <DividerHeading>Future Moves</DividerHeading>
+          <div className="grid flex-1 grid-cols-[auto_1fr] gap-1 items-center gap-x-2">
+            {map(future, (move) => {
+              return (
+                <Fragment key={move.id}>
+                  <div className="text-gray-500 text-sm">
+                    Level {move.learnAtLevel}
+                  </div>
+                  <FighterMove fighter={c} move={move} />
+                </Fragment>
+              )
+            })}
+          </div>
+        </>
+      )}
       {SHOW_JSON && (
         <>
           <DividerHeading>JSON</DividerHeading>
