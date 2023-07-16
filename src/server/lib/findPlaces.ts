46c12e786a93d0f5b68a96f4823aa6ffd2722c48
Tristan Rechenberger <rechenberger@me.com>
2023-07-16 19:40:31 +0200

    🎉 places

--- src/server/lib/findPlaces.ts
+++ src/server/lib/findPlaces.ts
@@ -1,4 +1,5 @@
 import { Client } from "@googlemaps/google-maps-services-js"
+import { PlaceType } from "@prisma/client"
 import { RADIUS_IN_KM_SCAN_PLACES } from "~/config"
 import { env } from "~/env.mjs"
 import { type LatLng } from "../schema/LatLng"
@@ -14,9 +15,24 @@ export const findPlaces = async ({ location }: { location: LatLng }) => {
     },
     timeout: 1000, // milliseconds
   })
-  if (result.data.status === "OK") {
-    console.log(result.data.results)
-  } else {
+  if (result.data.status !== "OK") {
     console.log(result.data.error_message)
+    throw new Error(`Google Maps API error: ${result.data.error_message}`)
   }
+
+  const results = result.data.results
+
+  const places = results.map((p) => {
+    return {
+      name: p.name,
+      lat: p.geometry?.location.lat,
+      lng: p.geometry?.location.lng,
+      type: PlaceType.CARE_CENTER,
+      googlePlaceId: p.place_id,
+    }
+  })
+
+  console.log(places)
+
+  return places
 }
