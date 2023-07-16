6eac1f37330760b2308d0024673066c528641e0d
Tristan Rechenberger <rechenberger@me.com>
2023-07-16 21:36:12 +0200

    🚑

--- src/server/lib/findPlaces.ts
+++ src/server/lib/findPlaces.ts
@@ -6,46 +6,51 @@ import { env } from "~/env.mjs"
 import { type LatLng } from "../schema/LatLng"
 
 export const findPlaces = async ({ location }: { location: LatLng }) => {
-  const client = new Client({})
-  const result = await client.placesNearby({
-    params: {
-      location, // Replace with player's location
-      radius: RADIUS_IN_KM_SCAN_PLACES * 1000, // Search within a 5km radius
-      type: "veterinary_care", // Search for veterinary clinics
-      key: env.GOOGLE_MAPS_PLACES_API_KEY,
-    },
-    timeout: 1000, // milliseconds
-  })
-  if (result.data.status !== "OK") {
-    console.log(result.data.error_message)
-    throw new Error(`Google Maps API error: ${result.data.error_message}`)
-  }
-
-  const results = result.data.results
-
-  const places = results.map((p) => {
-    return {
-      name: p.name,
-      lat: p.geometry?.location.lat,
-      lng: p.geometry?.location.lng,
-      type: PlaceType.CARE_CENTER,
-      googlePlaceId: p.place_id,
-    }
-  })
-
-  const goodPlaces = flatMap(places, (p) => {
-    if (!p.lat || !p.lng || !p.googlePlaceId) {
-      return []
-    }
-    return [
-      {
-        ...p,
-        lat: p.lat!,
-        lng: p.lng!,
-        googlePlaceId: p.googlePlaceId!,
+  try {
+    const client = new Client({})
+    const result = await client.placesNearby({
+      params: {
+        location, // Replace with player's location
+        radius: RADIUS_IN_KM_SCAN_PLACES * 1000, // Search within a 5km radius
+        type: "veterinary_care", // Search for veterinary clinics
+        key: env.GOOGLE_MAPS_PLACES_API_KEY,
       },
-    ]
-  })
+      timeout: 1000, // milliseconds
+    })
+    if (result.data.status !== "OK") {
+      console.log(result.data.error_message)
+      throw new Error(`Google Maps API error: ${result.data.error_message}`)
+    }
 
-  return goodPlaces
+    const results = result.data.results
+
+    const places = results.map((p) => {
+      return {
+        name: p.name,
+        lat: p.geometry?.location.lat,
+        lng: p.geometry?.location.lng,
+        type: PlaceType.CARE_CENTER,
+        googlePlaceId: p.place_id,
+      }
+    })
+
+    const goodPlaces = flatMap(places, (p) => {
+      if (!p.lat || !p.lng || !p.googlePlaceId) {
+        return []
+      }
+      return [
+        {
+          ...p,
+          lat: p.lat!,
+          lng: p.lng!,
+          googlePlaceId: p.googlePlaceId!,
+        },
+      ]
+    })
+
+    return goodPlaces
+  } catch (e) {
+    console.error(e)
+    return []
+  }
 }
