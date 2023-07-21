f8a18751469110a8eb41528830adc56b2621a3ef
Tristan Rechenberger <rechenberger@me.com>
2023-07-21 15:26:19 +0200

    ✨ MainNavigationButton

--- src/client/MainActionButtons.tsx
+++ src/client/MainActionButtons.tsx
@@ -1,4 +1,5 @@
 import { BattleViewButton } from "./BattleViewButton"
+import { MainNavigationButton } from "./MainNavigationButton"
 import { MyCatchesButton } from "./MyCatchesButton"
 import { ScanButton } from "./ScanButton"
 import { SettingsButton } from "./SettingsButton"
@@ -8,17 +9,22 @@ export const MainActionButtons = () => {
   return (
     <>
       <div
-        className="absolute inset-x-2 bottom-1 z-40 flex flex-row justify-center gap-4"
+        className="absolute inset-x-2 bottom-1 z-40 flex flex-row justify-center"
         style={{
           bottom: "calc(env(safe-area-inset-bottom) + 0.25rem)",
         }}
       >
-        <ScanButton />
-        <BattleViewButton />
-        <MyCatchesButton />
-        <SocialOverviewButton />
-        <SettingsButton />
-        {/* <TeleportToCurrentPositionButton /> */}
+        <div className="flex flex-col gap-4 w-min">
+          <MainNavigationButton />
+          <div className="flex flex-row gap-4 justify-center ">
+            <ScanButton />
+            <BattleViewButton />
+            <MyCatchesButton />
+            <SocialOverviewButton />
+            <SettingsButton />
+            {/* <TeleportToCurrentPositionButton /> */}
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/client/MainNavigationButton.tsx
@@ -0,0 +1,9 @@
+import { Button } from "./shadcn/ui/button"
+
+export const MainNavigationButton = () => {
+  return (
+    <>
+      <Button className="rounded-full">Navigate</Button>
+    </>
+  )
+}
