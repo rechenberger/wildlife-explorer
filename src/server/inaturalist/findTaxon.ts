836944296bd1a831fa2b9aff5b8edba80675a699
Tristan Rechenberger <rechenberger@me.com>
2023-07-22 15:11:10 +0200

    ✨ taxons

--- src/server/inaturalist/findTaxon.ts
+++ src/server/inaturalist/findTaxon.ts
@@ -1,8 +1,31 @@
 import { findTaxons } from "./findTaxons"
 
-export const findTaxon = async ({ taxonId }: { taxonId: number }) => {
-  const taxons = await findTaxons({ taxonIds: [taxonId] })
-  const taxon = taxons.find((t) => t.taxonId === taxonId)
-  if (!taxon) throw new Error(`taxon not found: ${taxonId}`)
-  return taxon
+let taxonIdsBatch: number[] = []
+let timer: NodeJS.Timeout | null = null
+let taxonPromises: { [key: number]: (value: any) => void } = {}
+
+export const findTaxon = ({ taxonId }: { taxonId: number }) => {
+  return new Promise((resolve, reject) => {
+    taxonIdsBatch.push(taxonId)
+    taxonPromises[taxonId] = resolve
+
+    if (!timer) {
+      timer = setTimeout(async () => {
+        try {
+          const taxons = await findTaxons({ taxonIds: taxonIdsBatch })
+          taxonIdsBatch.forEach((id) => {
+            const taxon = taxons.find((t) => t.taxonId === id)
+            if (!taxon) throw new Error(`taxon not found: ${id}`)
+            taxonPromises[id]!(taxon)
+          })
+        } catch (error) {
+          reject(error)
+        } finally {
+          taxonIdsBatch = []
+          taxonPromises = {}
+          timer = null
+        }
+      }, 2000)
+    }
+  })
 }
