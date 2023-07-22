0d6a56647636da33de2a496e807b5ed2dc5a496a
Tristan Rechenberger <rechenberger@me.com>
2023-07-22 12:37:22 +0200

    ✨ taxons

---
+++ src/server/inaturalist/importTaxon.ts
@@ -0,0 +1,46 @@
+import { Dex } from "@pkmn/dex"
+import { last } from "lodash-es"
+import { type MyPrismaClient } from "../db"
+import { taxonMappingByAncestors } from "../lib/battle/taxonMappingByAncestors"
+import { findTaxon } from "./findTaxon"
+
+export const importTaxon = async ({
+  prisma,
+  taxonId,
+  userId,
+}: {
+  prisma: MyPrismaClient
+  taxonId: number
+  userId: string
+}) => {
+  const id = taxonId
+  const existing = await prisma.taxon.findUnique({
+    where: {
+      id,
+    },
+  })
+  if (existing) return
+
+  const metadata = await findTaxon({ taxonId })
+  const mapping = taxonMappingByAncestors(metadata.taxonAncestorIds)
+  const fighterSpeciesName = mapping.pokemon
+  const fighterSpeciesNum = Dex.species.get(fighterSpeciesName)?.num
+  if (!fighterSpeciesNum) throw new Error("no fighterSpeciesNum")
+  const mainMapping = mapping.taxonId
+  const isMainMapping = mapping.taxonId === taxonId
+  if (!isMainMapping) {
+    await importTaxon({ prisma, taxonId: mainMapping, userId })
+  }
+  const foundById = userId
+  const ancestorMappedId = isMainMapping ? null : mainMapping
+  const ancestorId = last(metadata.taxonAncestorIds) ?? null
+  await prisma.taxon.create({
+    data: {
+      id,
+      metadata,
+      foundById,
+      ancestorMappedId,
+      ancestorId,
+    },
+  })
+}
