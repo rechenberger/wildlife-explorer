e734e53bd9ba0c339a33012daf30d774ce8b1e0d
Tristan Rechenberger <rechenberger@me.com>
2023-07-22 12:21:03 +0200

    ✨ taxons

--- src/server/inaturalist/findTaxon.ts
+++ src/server/inaturalist/findTaxon.ts
@@ -1,4 +1,5 @@
 import { z } from "zod"
+import { TaxonMetadata } from "../schema/TaxonMetadata"
 import { Taxon } from "./schema"
 
 export const findTaxon = async ({ taxonId }: { taxonId: number }) => {
@@ -14,5 +15,30 @@ export const findTaxon = async ({ taxonId }: { taxonId: number }) => {
     results: z.array(Taxon),
   })
   const parsed = schema.parse(data)
-  return parsed
+  const taxon = parsed.results[0]
+  if (!taxon) throw new Error("no taxon found")
+
+  const metadata = {
+    taxonId: taxon.id,
+    taxonImageUrlSquare: taxon.default_photo?.square_url ?? null,
+    taxonImageUrlMedium: taxon.default_photo?.medium_url ?? null,
+    taxonImageUrlSmall: taxon.default_photo?.url ?? null,
+    taxonImageLicenseCode: taxon.default_photo?.license_code ?? null,
+    taxonImageAttribution: taxon.default_photo?.attribution ?? null,
+    taxonWikiUrl: taxon.wikipedia_url,
+
+    // taxon:
+    taxonAncestorIds: taxon.ancestor_ids,
+    taxonSearchRank: taxon.universal_search_rank,
+    taxonRank: taxon.rank,
+    taxonObservationsCount: taxon.observations_count,
+    taxonName: taxon.name,
+    taxonCommonName: taxon.english_common_name ?? null,
+    taxonLocaleNames: {
+      en: taxon.english_common_name ?? null,
+      de: taxon.preferred_common_name ?? null,
+    },
+  } satisfies TaxonMetadata
+
+  return metadata
 }
