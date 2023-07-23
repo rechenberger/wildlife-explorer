f72b9c05f497eaa9547ceac7358dbe667e50a6df
Tristan Rechenberger <rechenberger@me.com>
2023-07-23 12:19:53 +0200

    🐛

--- src/server/api/routers/navigation.router.ts
+++ src/server/api/routers/navigation.router.ts
@@ -98,7 +98,7 @@ export const navigationRouter = createTRPCRouter({
       const { timingLegs, totalDistanceInMeter } = calcTimingLegs(navigation)
       navigation.totalDistanceInMeter = totalDistanceInMeter
 
-      if (totalDistanceInMeter <= 1 || totalDurationInSeconds <= 1) {
+      if (totalDistanceInMeter <= 1 || totalDurationInSeconds <= 0.1) {
         throw new TRPCError({
           code: "BAD_REQUEST",
           message: "You are already at your destination",
