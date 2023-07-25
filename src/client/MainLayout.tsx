244048ee509f92c6ac034b81a6dbda085e614db4
Tristan Rechenberger <rechenberger@me.com>
2023-07-25 15:04:09 +0200

    ✨ teampilot widget

--- src/client/MainLayout.tsx
+++ src/client/MainLayout.tsx
@@ -18,6 +18,13 @@ export const MainLayout = ({ children }: { children?: ReactNode }) => {
           content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
         />
         <link rel="manifest" href="/manifest.json" />
+        <script
+          defer
+          src="http://localhost:3000/widgetScript.js"
+          data-launchpad-slug-id="wilda-afbd2c9d288c7494142cb0fe15a943dc"
+          data-icon-bg="#508090"
+          data-icon-color="#fff"
+        />
       </Head>
       <main className="relative flex h-[100svh] w-full flex-col items-center justify-center bg-gradient-to-br  from-cyan-500 to-amber-600 text-white">
         <MainTopNav />
