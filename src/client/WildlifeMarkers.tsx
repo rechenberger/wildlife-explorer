7d2fd633e45d039ed07e9c82919f499bbeba9aca
Tim Weiskopf <weiskopf@sodefa.de>
2023-07-28 16:46:11 +0200

    ⚡️ memo markers

--- src/client/WildlifeMarkers.tsx
+++ src/client/WildlifeMarkers.tsx
@@ -1,16 +1,17 @@
 import { useStore } from "jotai"
 import { Loader2 } from "lucide-react"
+import { Fragment, memo } from "react"
 import { WildlifeMarker } from "./WildlifeMarker"
 import { isNavigatingAtom } from "./useActiveNavigation"
 import { useWildlife } from "./useWildlife"
 
-export const WildlifeMarkers = () => {
+export const WildlifeMarkers = memo(() => {
   const { wildlife, isFetching } = useWildlife()
   const store = useStore()
   const isNavigating = store.get(isNavigatingAtom)
-
+  WildlifeMarkers.displayName = "WildlifeMarkers"
   return (
-    <>
+    <Fragment>
       {isFetching && (
         <div className="absolute right-2 top-2 z-50 animate-spin text-amber-400">
           <Loader2 />
@@ -25,6 +26,6 @@ export const WildlifeMarkers = () => {
           />
         )
       })}
-    </>
+    </Fragment>
   )
-}
+})
