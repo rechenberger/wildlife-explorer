4ed26746c2a8aa5c51eccfca07e979c26c875e85
Tristan Rechenberger <rechenberger@me.com>
2023-07-30 21:50:13 +0200

    💄

--- src/client/PlaceViewModal.tsx
+++ src/client/PlaceViewModal.tsx
@@ -2,7 +2,6 @@ import NiceModal, { useModal } from "@ebay/nice-modal-react"
 import { PlaceView } from "./PlaceView"
 import { cn } from "./cn"
 import { Dialog, DialogContent } from "./shadcn/ui/dialog"
-import { ScrollArea } from "./shadcn/ui/scroll-area"
 
 export const PlaceViewModal = NiceModal.create<{ placeId: string }>(
   ({ placeId }) => {
@@ -22,21 +21,20 @@ export const PlaceViewModal = NiceModal.create<{ placeId: string }>(
         <DialogContent
           className={cn(
             "border-0 bg-white pt-3",
-            "rounded-t-lg max-sm:bottom-0 max-sm:top-auto max-sm:translate-y-0 max-sm:p-4 max-sm:pt-3"
+            "rounded-t-lg max-sm:bottom-0 max-sm:top-auto max-sm:translate-y-0 max-sm:p-4 max-sm:pt-3",
+            "max-h-[calc(100svh-100px)] overflow-auto"
           )}
           style={{
             paddingBottom: "calc(env(safe-area-inset-bottom) + 1.5rem)",
           }}
         >
-          <ScrollArea className="max-h-[calc(100svh-100px)]">
-            <PlaceView
-              placeId={placeId}
-              close={() => {
-                modal.reject()
-                modal.remove()
-              }}
-            />
-          </ScrollArea>
+          <PlaceView
+            placeId={placeId}
+            close={() => {
+              modal.reject()
+              modal.remove()
+            }}
+          />
         </DialogContent>
       </Dialog>
     )
