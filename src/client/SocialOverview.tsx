6c02e5dffacbc3814d50ad135a7d9653beff1369
Tristan Rechenberger <rechenberger@me.com>
2023-07-30 22:15:03 +0200

    💄

--- src/client/SocialOverview.tsx
+++ src/client/SocialOverview.tsx
@@ -12,7 +12,7 @@ import { useStartTrade } from "./useStartTrade"
 
 export const SocialOverview = () => {
   const { playerId } = usePlayer()
-  const { data: players } = api.social.getOverview.useQuery(
+  const { data: players, isLoading } = api.social.getOverview.useQuery(
     {
       playerId: playerId!,
     },
@@ -32,14 +32,22 @@ export const SocialOverview = () => {
 
   const mapFlyTo = useMapFlyTo()
 
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center py-12 text-center text-sm opacity-60">
+        Loading...
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="pb-4 px-4 border-b">Players</div>
       <div className="flex flex-col">
         {map(players, (player) => (
           <Fragment key={player.id}>
-            <div className="flex flex-row items-center gap-2 border-b p-4 text-left hover:bg-gray-100">
-              <User size={24} />
+            <div className="flex flex-row items-center gap-2 border-b py-4 px-4 text-left hover:bg-gray-100">
+              <User size={24} className="hidden sm:flex" />
               <div className="flex flex-col">
                 <div>{player.name}</div>
                 <div className="text-xs opacity-60">
@@ -48,14 +56,14 @@ export const SocialOverview = () => {
               </div>
               <div className="flex-1" />
               <div className="flex-1" />
-              <div className="flex flex-col text-xs opacity-60 text-right">
+              <div className="flex flex-col text-xs opacity-60 text-right whitespace-nowrap">
                 <div>{player._count.foundWildlife} found</div>
                 <div>{player._count.catches} caught</div>
                 <div>{player._count.battleParticipations} defeated</div>
               </div>
-              <div className="flex flex-col gap-1">
+              <div className="flex flex-col gap-0.5">
                 <button
-                  className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-0.5 border"
+                  className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-1 border"
                   onClick={() => {
                     NiceModal.hide(SocialOverviewModal)
                     mapFlyTo({ center: player })
@@ -67,7 +75,7 @@ export const SocialOverview = () => {
                 {playerId !== player.id && (
                   <>
                     <button
-                      className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-0.5 border"
+                      className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-1 border"
                       onClick={() => {
                         if (!playerId) return
                         startPvp({
@@ -80,7 +88,7 @@ export const SocialOverview = () => {
                       <div>Battle</div>
                     </button>
                     <button
-                      className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-0.5 border"
+                      className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-1 border"
                       disabled={startTradeIsLoading}
                       onClick={() => {
                         if (startTradeIsLoading) return
@@ -96,7 +104,7 @@ export const SocialOverview = () => {
                 )}
                 {player.metadata?.activeBattleId && (
                   <button
-                    className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-0.5 border"
+                    className="flex flex-row gap-1 rounded text-xs items-center bg-black text-white px-2 py-1 border"
                     onClick={() => {
                       const battleId = player.metadata?.activeBattleId
                       if (!battleId) return
--- src/client/SocialOverviewModal.tsx
+++ src/client/SocialOverviewModal.tsx
@@ -20,9 +20,9 @@ export const SocialOverviewModal = NiceModal.create<{}>(({}) => {
       <DialogContent
         className={cn(
           "border-0 bg-white pt-3",
-          "rounded-t-lg max-sm:bottom-0 max-sm:top-auto max-sm:translate-y-0 max-sm:p-4 max-sm:pt-3",
-          "px-0",
-          "max-h-96 overflow-auto"
+          "rounded-t-lg max-sm:bottom-0 max-sm:top-auto max-sm:translate-y-0 max-sm:pt-3",
+          "px-0 sm:px-0",
+          "max-h-[calc(100svh-200px)] overflow-auto"
         )}
         style={{
           paddingBottom: "calc(env(safe-area-inset-bottom) + 1.5rem)",
